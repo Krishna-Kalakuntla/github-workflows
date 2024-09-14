bc1530c6240a69d95718426994fbe5af49e39bbf
Krishna Kalakuntla <krishna.kalakuntla15@gmail.com>
2024-09-14 20:14:45 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@ echo "My first Github Action Job"
 ls -a
 cat README.md
 echo "Generate ASCII artwork"
-sudo apt-get install cowsay -y 
+sudo apt-get install cowsay -y
+echo "########Cowsay installed#########"
 cowsay -f dragon "Dragon king" >> dragon.txt
